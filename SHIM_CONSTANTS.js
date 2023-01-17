30f0ba401913b0a342bb280c04a475b59b58faeb
Jarron-Ng <39303902+Jarron-Ng@users.noreply.github.com>
2023-01-17 23:23:05 +0100

    Delete SHIM_CONSTANTS.js

--- SHIM_CONSTANTS.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.FIREJET_SAVE_DATA_PATH = exports.ARBITRARY_COMPONENT_FOLDER_PATH = void 0;
-exports.ARBITRARY_COMPONENT_FOLDER_PATH = 'src/app/components';
-exports.FIREJET_SAVE_DATA_PATH = 'src/app/components/firejet-save-data';
